377e3a6d1cd354d66a5d730047533c1ccc18c7a8
rickcmc02 <aa87565115@gmail.com>
2021-03-11 16:35:53 +0900

    arrange components into component folder

---
+++ src/components/TOC.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TOC extends Component {
+    render() {
+      return (
+        <nav>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">Javascript</a></li>
+        </nav>
+      )
+    }
+  }
+
+export default TOC;
