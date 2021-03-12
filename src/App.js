c1dea8aa9e74966f09267b6af82616e9e7b901aa
rickcmc02 <aa87565115@gmail.com>
2021-03-12 13:16:07 +0900

    prevent duplication in create btn by shouldcomponentupdate

--- src/App.js
+++ src/App.js
@@ -47,9 +47,6 @@ class App extends Component {
       _article = <CreateContent onSubmit={function(_title, _desc) {
         // add content to this.state.contents
         this.max_content_id = this.max_content_id + 1;
-        // this.state.contents.push(
-        //   {id:this.max_content_id, title:_title, desc:_desc}
-        // );
         var _contents = this.state.contents.concat(
           {id:this.max_content_id, title:_title, desc:_desc}
         )
